7211157e65869bac5c009c7fdf685a437cfa0164
Matveiiy <51031652+Matveiiy@users.noreply.github.com>
2022-07-02 21:19:11 +0300

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -2292,45 +2292,6 @@ namespace ChessEngine
             //char sq2 = (str[2]-'a') * 8 + (str[3]-'1');
             
         }
-        int input_waiting()
-        {
-            #ifndef WIN32
-                fd_set readfds;
-                struct timeval tv;
-                FD_ZERO (&readfds);
-                FD_SET (fileno(stdin), &readfds);
-                tv.tv_sec=0; tv.tv_usec=0;
-                select(16, &readfds, 0, 0, &tv);
-
-                return (FD_ISSET(fileno(stdin), &readfds));
-            #else
-                static int init = 0, pipe;
-                static HANDLE inh;
-                DWORD dw;
-                if (!init)
-                {
-                    init = 1;
-                    inh = GetStdHandle(STD_INPUT_HANDLE);
-                    pipe = !GetConsoleMode(inh, &dw);
-                    if (!pipe)
-                    {
-                        SetConsoleMode(inh, dw & ~(ENABLE_MOUSE_INPUT|ENABLE_WINDOW_INPUT));
-                        FlushConsoleInputBuffer(inh);
-                    }
-                }
-                if (pipe)
-                {
-                    if (!PeekNamedPipe(inh, NULL, 0, NULL, &dw, NULL)) return 1;
-                    return dw;
-                }
-                else
-                {
-                    GetNumberOfConsoleInputEvents(inh, &dw);
-                    return dw <= 1 ? 0 : dw;
-                }
-            #endif
-        }
-
         void uci_loop(SearchEngine& engine) {
             // print engine info
             engine.board.Reset();
